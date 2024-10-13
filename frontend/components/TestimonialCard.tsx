4e4dc868f38321da3bd05881c4583913b2168a7f
Bhoomaahamso <bhoomaahamso@gmail.com>
2024-10-14 00:11:50 +0530

    final  v2

--- frontend/components/TestimonialCard.tsx
+++ frontend/components/TestimonialCard.tsx
@@ -2,9 +2,9 @@ import { Separator } from "@/components/ui/separator";
 import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import Image from "next/image";
 
-function TestimonialCard({ name, percentile, text, video }) {
+function TestimonialCard({ name, percentile, text, video }: {name: string, percentile: string, text: string, video: string}) {
   const regex = /\/embed\/([a-zA-Z0-9_-]+)/;
-  const val = video.match(regex)[1];
+  const val = video.match(regex)?.[1];
   return (
     <div className="p-8 border-t-[#335eea] rounded">
       {/* <div className=""><Image /></div> */}
@@ -33,10 +33,11 @@ function TestimonialCard({ name, percentile, text, video }) {
                 // height="315"
                 src={video}
                 title="YouTube video player"
-                frameborder="0"
+              
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin"
-                allowfullscreen
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
               ></iframe>
             </div>
           </DialogContent>
